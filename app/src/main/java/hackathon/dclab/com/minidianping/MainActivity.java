ad65883adf0a36be3e88f446a4742f012a139780
Yongfeng <kymtwyf@gmail.com>
2015-05-21 22:17:44 +0800

    NetUtils

--- app/src/main/java/hackathon/dclab/com/minidianping/MainActivity.java
+++ app/src/main/java/hackathon/dclab/com/minidianping/MainActivity.java
@@ -2,12 +2,14 @@ package hackathon.dclab.com.minidianping;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private static String TAG = "MainActivity";
+//    private static String URL = "http://www.baidu.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +21,11 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -36,4 +41,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/hackathon/dclab/com/minidianping/Services.java
@@ -0,0 +1,11 @@
+package hackathon.dclab.com.minidianping;
+
+/**
+ * Created by Yongfeng on 15/5/21.
+ * @description 服务类，负责提供从service得到数据并返回相应实体
+ *
+ */
+public class Services {
+    public static final String host = "";
+
+}
